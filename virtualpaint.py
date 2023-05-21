151b713624c826395bbb763e1d0f4a21aa115c06
tanim95 <tanimrahman78@gmail.com>
2023-05-21 21:09:20 +0600

    done virtual paint,in this project i used a green pen.

--- virtualpaint.py
+++ virtualpaint.py
@@ -46,42 +46,102 @@ def show_video():
 
 # show_video()
 
+# def show_vid():
+#     video = cv2.VideoCapture(0)
+#     video.set(3, 640)
+#     video.set(4, 480)
+
+#     # 3 diffrent color pen mask
+#     my_colors = [[35, 93, 59, 94, 179, 179], [
+#         50, 80, 70, 100, 120, 125], [45, 80, 65, 100, 150, 150]]
+
+#     def find_color(img, colors):
+#         hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+#         masks = []
+#         for color in colors:
+#             lower = np.array(color[0:3])
+#             upper = np.array(color[3:6])
+#             mask = cv2.inRange(hsv_img, lower, upper)
+#             masks.append(mask)
+
+#         combined_mask = cv2.bitwise_or(*masks)
+#         find_contours(img, combined_mask)
+#         cv2.imshow('Combined Mask', combined_mask)
+#         cv2.waitKey(1)
+
+#     def find_contours(img, mask):
+#         contours, _ = cv2.findContours(
+#             mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+#         for contour in contours:
+#             x, y, w, h = cv2.boundingRect(contour)
+#             cv2.drawContours(img, [contour], -1, (0, 255, 0), 2)
+#             cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
+#     while True:
+#         ret, frame = video.read()
+#         copied_img = frame.copy()
+#         find_color(copied_img, my_colors)
+
+#         if not ret:
+#             break
+
+#         cv2.imshow('video', copied_img)
+#         if cv2.waitKey(1) & 0xFF == ord('q'):
+#             break
+#     video.release()
+#     cv2.destroyAllWindows()
+
+
+# show_vid()
+
 def show_vid():
     video = cv2.VideoCapture(0)
     video.set(3, 640)
     video.set(4, 480)
 
-    # Colors for detection
-    my_colors = [[35, 93, 59, 94, 179, 179], [
-        50, 80, 70, 100, 120, 125], [45, 80, 65, 100, 150, 150]]
+    # single color pen mask
+    my_colors = [35, 93, 59, 94, 179, 179]
+    colr = (0, 255, 0)
+    moving_points = []  # [x,y]
 
-    def find_color(img, colors):
+    def find_color(img, color):
+        newpoints = []
         hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-        masks = []
-        for color in colors:
-            lower = np.array(color[0:3])
-            upper = np.array(color[3:6])
-            mask = cv2.inRange(hsv_img, lower, upper)
-            masks.append(mask)
-
-        combined_mask = cv2.bitwise_or(*masks)
-        find_contours(img, combined_mask)
-        cv2.imshow('Combined Mask', combined_mask)
+        lower = np.array(color[0:3])
+        upper = np.array(color[3:6])
+        mask = cv2.inRange(hsv_img, lower, upper)
+        x, y = find_contours(img, mask)
+        cv2.circle(img, (x, y), 10, (0, 255, 0), cv2.FILLED)
+        if (x != 0 and y != 0):
+            newpoints.append([x, y])
+        cv2.imshow('Mask', mask)
         cv2.waitKey(1)
+        return newpoints
 
     def find_contours(img, mask):
         contours, _ = cv2.findContours(
             mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
+        x, y, w, h = 0, 0, 0, 0
         for contour in contours:
             x, y, w, h = cv2.boundingRect(contour)
-            cv2.drawContours(img, [contour], -1, (0, 255, 0), 2)
-            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
+            # cv2.drawContours(img, [contour], -1, (0, 255, 0), 2)
+            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        return x+w // 2, y
+
+    def draw(img, points, color):
+        for point in points:
+            cv2.circle(img, (point[0], point[1]), 10, color, cv2.FILLED)
 
     while True:
         ret, frame = video.read()
         copied_img = frame.copy()
-        find_color(copied_img, my_colors)
+        newpoints = find_color(copied_img, my_colors)
+        if newpoints is not None:
+            for p in newpoints:
+                moving_points.append(p)
+        if moving_points is not None:
+            draw(copied_img, moving_points, colr)
 
         if not ret:
             break
@@ -89,6 +149,7 @@ def show_vid():
         cv2.imshow('video', copied_img)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
+
     video.release()
     cv2.destroyAllWindows()
 
