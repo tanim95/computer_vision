2b28402e69e14c0fb9b03b74bdc3cea132493d8e
tanim95 <tanimrahman78@gmail.com>
2023-12-20 10:56:27 +0600

    .

--- face.py
+++ face.py
@@ -28,40 +28,11 @@ while True:
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
+cap.release()
+cv2.destroyAllWindows()
 
 # haarcascade = cv2.data.haarcascades + 'haarcascade_frontalface_default.xml'
 # with open(haarcascade, 'r') as file:
 #     xml_content = file.read()
 # with open('face_detection.xml', 'w') as output_file:
 #     output_file.write(xml_content)
-
-
-# ..........................................Optical flow..........
-
-# cap = cv2.VideoCapture(0)
-# ret, frame1 = cap.read()
-# prvs = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
-# hsv = np.zeros_like(frame1)
-# hsv[..., 1] = 255
-
-# while True:
-#     ret, frame2 = cap.read()
-#     next_frame = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
-
-#     flow = cv2.calcOpticalFlowFarneback(
-#         prvs, next_frame, None, 0.5, 3, 15, 3, 5, 1.2, 0)
-
-#     mag, ang = cv2.cartToPolar(flow[..., 0], flow[..., 1])
-#     hsv[..., 0] = ang * 180 / np.pi / 2
-#     hsv[..., 2] = cv2.normalize(mag, None, 0, 255, cv2.NORM_MINMAX)
-
-#     rgb = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
-
-#     cv2.imshow('Optical Flow', rgb)
-#     k = cv2.waitKey(30) & 0xFF
-#     if k == 27:  # Press 'Esc' to exit
-#         break
-#     prvs = next_frame
-
-# cap.release()
-# cv2.destroyAllWindows()
